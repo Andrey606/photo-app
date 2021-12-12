5089ec1fd0ba2d9e601c254379d52be6015ff73a
Andrey606 <andreykuluev96@gmail.com>
2021-12-12 21:46:05 +0200

    sdsd

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => 'apikey',
+  :password => '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',
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
