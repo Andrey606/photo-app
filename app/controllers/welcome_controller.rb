65a20c4f0876987a4269d2fbd4ae6e88d91f88ab
Andrey606 <andreykuluev96@gmail.com>
2022-01-04 21:26:42 +0200

    modife welkome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,29 +1,7 @@
-# using SendGrid's Ruby Library
-# https://github.com/sendgrid/sendgrid-ruby
-require 'sendgrid-ruby'
-include SendGrid
-
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    send_email
-  end
-  
-  private
 
-  def send_email
-    from = SendGrid::Email.new(email: 'andreykuluev96@gmail.com')
-    to = SendGrid::Email.new(email: 'akuluev@ukr.net')
-    subject = 'Sending with Twilio SendGrid is no Fun'
-    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = SendGrid::Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: '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')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    # puts response.parsed_body
-    puts response.headers
   end
 end
